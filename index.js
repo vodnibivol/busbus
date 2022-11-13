a10088b12d29c7cb075b1d552ded345e735e9016
vodnibivol <vodnibivol@gmail.com>
2022-11-13 16:34:02 +0100

    major update: added bus lines

--- index.js
+++ index.js
@@ -20,8 +20,22 @@ app.get('/', (_, res) => {
   res.render('form');
 });
 
-app.get('/map', (_, res) => {
-  res.render('map');
+app.get('/map', (req, res) => {
+  const routeNos = req.query.line.split(','); // ["2", "9"]
+  const routeF = JSON.parse(fs.readFileSync(path.resolve('db', 'routes', 'routes.json')));
+
+  let routeFiles = routeF.filter((fn) => {
+    return routeNos.some((no) => fn.filename.startsWith(no + '_'));
+  });
+
+  routeFiles = routeFiles.map(f => {
+    const coords = JSON.parse(fs.readFileSync(path.resolve('db', 'routes', f.filename)))
+    return {...f, coordinates: coords};
+  })
+
+  // console.log(routeFiles.map(f => f.trip_id))
+
+  res.render('map', { routes: routeFiles });
 });
 
 // --- API
@@ -42,6 +56,34 @@ app.get('/api/getStationData/:stationId', async (req, res) => {
   const url = `https://www.lpp.si/lpp/ajax/1/${req.params.stationId}`;
   const stationData = await getJSON(url);
   res.json(stationData);
+});
+
+app.get('/api/getRoute/:routeNo', (req, res) => {
+  const routeNo = req.params.routeNo.split(','); // ["2", "11", "11b"]
+  const filenames = fs.readdirSync(path.resolve('db', 'routes'));
+  const routeFiles = filenames.filter((fn) => {
+    return routeNo.some((no) => fn.startsWith(no + '_'));
+  });
+
+  console.log(routeFiles);
+
+  return res.end();
+
+  let coords = {};
+
+  if (routeFiles.length) {
+    const file = JSON.parse(fs.readFileSync(path.resolve('db', route)));
+    const coords = file.features[0].geometry.coordinates;
+
+    const depth = JSON.stringify(coords)
+      .substring(0, 5)
+      .split('')
+      .reduce((acc, cur) => {
+        return cur === '[' ? ++acc : acc;
+      }, 0);
+
+    res.json(coords.flat(depth - 2).map(([lat, lon]) => [lon, lat]));
+  } else res.status(404).end('no routes found.');
 });
 
 // --- f(x)
--- public/map.js
+++ public/map.js
@@ -1,109 +1,7 @@
-class Buses {
-  constructor(routes) {
-    this.routes = routes;
-    this.buses = [];
-    this.trips = [];
-
-    this.dstore = new Store('BUSBUS');
-
-    this.i = 0;
-    // this.routes = [1, 2, 9, 11, 25, 19, 27, 6];
-    // this.routes = [2, '11b'];
-    console.log(this.routes);
-  }
-
-  updateTrips() {
-    this.trips = [...new Set(this.buses.map((b) => b.data.trip_id))]; //.sort();
-    this.buses.forEach((b) => (b.data.direction = this.trips.indexOf(b.data.trip_id)));
-  }
-
-  async update() {
-    this.routeNo = this.routes[this.i++ % this.routes.length];
-    await this.updateData();
-    this.updateTrips();
-    this.draw();
-  }
-
-  async updateData() {
-    // update buses data
-    let data = this.dstore.get('busdata');
-
-    if (!data) {
-      const r = await fetch(`/api/getBusData/${this.routeNo}`);
-      const j = await r.json();
-      data = j.data;
-      this.dstore.set('busdata' + this.routeNo, data, this.dstore.SECOND * 5);
-    }
-
-    for (let busData of data) {
-      const id = busData.bus_unit_id;
-
-      let bus = this.buses.find((b) => b.id === id);
-      if (bus) bus.update(busData);
-      else this.buses.push(new Bus(id, busData));
-    }
-  }
-
-  draw() {
-    this.buses.forEach((b) => b.draw());
-  }
-}
-
-class Bus {
-  constructor(id, data) {
-    this.id = id;
-    this.data = null;
-    this.marker = null;
-
-    this.update(data);
-  }
-
-  get age() {
-    // when was bus data last updated (seconds)
-    return Math.round((new Date() - new Date(this.data.bus_timestamp)) / 1000);
-  }
-
-  get popupContent() {
-    return [
-      `- ${this.data.destination} (${this.data.route_number})`,
-      `- pred ${this.age} sekundami`,
-      `- [${this.data.bus_name}]`,
-    ].join('<br>');
-  }
-
-  update(data) {
-    // update (only data)
-    this.data = data;
-    this.latlon = [data.latitude, data.longitude];
-  }
-
-  draw() {
-    if (!this.marker) {
-      // first time
-      this.marker = L.marker(this.latlon, {
-        icon: Icons.bus,
-        rotationOrigin: 'center',
-      });
-
-      this.marker.addTo(map);
-      this.marker.on('click', () => this.marker.bindPopup(this.popupContent));
-    }
-
-    this.marker.setLatLng(new L.LatLng(...this.latlon));
-    this.marker.setRotationAngle(this.data.cardinal_direction - 90);
-    this.marker.bindPopup(this.popupContent);
-
-    this.marker._icon.style.opacity = this.age > 60 ? '0.5' : '1';
-    this.marker._icon.style.filter = 'hue-rotate(' + this.data.direction * 280 + 'deg)';
-  }
-
-  removeMarker() {
-    if (this.marker) map.removeLayer(this.marker);
-  }
-}
-
-// global map
-const map = L.map('map').setView([46.05, 14.52], 13);
+// global
+let map;
+let buses;
+let lines;
 
 const Main = (async function () {
   // init
@@ -111,6 +9,8 @@ const Main = (async function () {
 
   // f(x)
   function init() {
+    map = L.map('map').setView([46.05, 14.52], 13);
+
     initMap();
     L.control.locate().addTo(map);
 
@@ -119,12 +19,15 @@ const Main = (async function () {
     const r = params.get('line') || '';
     const routes = r.split(',').map((i) => i.trim());
 
-    const BUSES = new Buses(routes);
-    BUSES.update();
+    buses = new Buses(routes);
+    buses.update();
 
     setInterval(() => {
-      BUSES.update();
+      buses.update();
     }, 1000);
+
+    // lines
+    lines = new Lines(routes);
   }
 
   function initMap() {
