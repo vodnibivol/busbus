af42b69e55d18bb2a063a320c8a23b186b63723b
vodnibivol <vodnibivol@gmail.com>
2025-05-13 22:52:15 +0200

    notification priority change

--- js/collectData.js
+++ js/collectData.js
@@ -61,6 +61,7 @@ export async function collectData(req, res, next) {
       body: `${username}: ${stopName} (${time})`,
       headers: {
         Title: 'New Search',
+        Priority: 'low',
       },
     });
   }
