ea81dd31260aeaa55a166a8968ae24fea21c65af
vodnibivol <vodnibivol@gmail.com>
2025-05-15 15:55:43 +0200

    dodal opomnik o sporočilih

--- routes/msg.js
+++ routes/msg.js
@@ -21,9 +21,48 @@ router.get('/delete', async (req, res) => {
 });
 
 router.get('/open', async (req, res) => {
-  const updated = await DB.messages.updateAsync({ _id: req.query.id }, { $set: { openedOn: new Date().valueOf() } });
+  const updated = await DB.messages.updateAsync(
+    { _id: req.query.id },
+    { $set: { openedOn: new Date().valueOf() } },
+    { returnUpdatedDocs: true }
+  );
+
+  const doc = updated?.affectedDocuments;
+
+  if (doc) {
+    const time = new Date().getHours() + '.' + ('' + new Date(doc.openedOn).getMinutes()).padStart(2, '0');
+
+    fetch('https://ntfy.sh/busbus-admin-log', {
+      method: 'POST',
+      body: `${time}: ${capitalize(doc.recipient)} je odprl/a sporočilo: "${doc.content}".`,
+      headers: {
+        Title: encodeBase64('Sporočilo!'),
+        // Click: 'https://strojcek.ftp.sh/busbus/msg/send/',
+      },
+    });
+  }
+
   res.json({ updated });
 });
+
+// --- UTILS
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+function trimString(str, maxLength) {
+  try {
+    if (str.length <= maxLength) return str;
+    return str.slice(0, maxLength - 8) + '..' + str.slice(str.length - 6);
+  } catch (error) {
+    return null;
+  }
+}
+
+function encodeBase64(str) {
+  return '=?UTF-8?B?' + Buffer.from(str).toString('base64') + '?=';
+}
 
 // --- EXPORT
 
