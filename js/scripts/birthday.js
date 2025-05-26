95858420df5c2ff7a9b20edbdd528f4bed5e5eaa
vodnibivol <vodnibivol@gmail.com>
2025-05-26 19:22:52 +0200

    popravil heartShower, dodal rojstni dan za vse in dodal napis 'novo sporočilo'

--- js/scripts/birthday.js
+++ js/scripts/birthday.js
@@ -4,8 +4,6 @@ export default function () {
     return today.getDate() === day && today.getMonth() + 1 === month;
   }
 
-  if (!(isDate(24, 5) || isDate(25, 5) || isDate(26, 5) || isDate(27, 5))) return;
-
   document.querySelector('#input .reset').style.background = 'pink';
   document.querySelector('#bg').style.background = 'url("public/img/bg/birthday-3.jpg")';
   document.querySelector('#bg').style.backgroundSize = '250px';
@@ -15,12 +13,7 @@ export default function () {
     if (e.target.matches('#paketek')) {
       if (!window.confetti) return;
 
-      confetti({
-        particleCount: 30,
-        angle: 60 * (1 + dir),
-        spread: 55,
-        origin: { x: dir, y: 0.6 },
-      });
+      heartShower();
     }
   });
 
@@ -31,47 +24,89 @@ export default function () {
     s.src = 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js';
     document.body.appendChild(s);
 
-    // confetti on click
-    document.addEventListener('touchstart', fireCannon);
-    document.addEventListener('keydown', fireCannon);
-    document.addEventListener('mousedown', fireCannon);
+    s.onload = () => {
+      // confetti on click
+      document.addEventListener('touchstart', fireCannon);
+      document.addEventListener('keydown', fireCannon);
+      document.addEventListener('mousedown', fireCannon);
 
-    function fireCannon() {
-      const dir = Math.round(Math.random());
-      confetti({
-        particleCount: 30,
-        angle: 60 * (1 + dir),
-        spread: 55,
-        origin: { x: dir, y: 0.6 },
-      });
-    }
+      function fireCannon() {
+        const dir = Math.round(Math.random());
+        confetti({
+          particleCount: 30,
+          angle: 60 * (1 + dir),
+          spread: 55,
+          origin: { x: dir, y: 0.6 },
+        });
+      }
 
-    // start animation
+      // start animation
+      const duration = 5 * 1000;
+      const animationEnd = Date.now() + duration;
+      const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+      function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+
+      const interval = setInterval(function () {
+        const timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) return clearInterval(interval);
+
+        const particleCount = 50 * (timeLeft / duration);
+
+        confetti({
+          ...defaults,
+          particleCount,
+          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        });
+        confetti({
+          ...defaults,
+          particleCount,
+          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        });
+      }, 250);
+    };
+  })();
+
+  // -- HEART SHOWER ANIMATION
+  function heartShower() {
     const duration = 5 * 1000;
     const animationEnd = Date.now() + duration;
-    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+    let skew = 1;
+    let int = 0;
+    const colors = ['#26ccff', '#a25afd', '#ff5e7e', '#88ff5a', '#fcff42', '#ffa62d', '#ff36ff'];
 
     function randomInRange(min, max) {
       return Math.random() * (max - min) + min;
     }
 
-    const interval = setInterval(function () {
+    (function frame() {
       const timeLeft = animationEnd - Date.now();
+      skew = Math.max(0.8, skew - 0.001);
 
-      if (timeLeft <= 0) return clearInterval(interval);
+      if (++int % 3 === 0)
+        confetti({
+          particleCount: 1,
+          startVelocity: 0,
+          ticks: 500,
+          origin: {
+            x: randomInRange(0, 1),
+            // since particles fall down, skew start toward the top
+            y: 0,
+            // y: skew-0.2,
+          },
+          colors: [colors[int % colors.length]],
+          gravity: randomInRange(0.8, 1),
+          scalar: randomInRange(0.5, 1.2),
+          drift: randomInRange(-0.4, 0.4), // 0.4
+          // flat: true,
+        });
 
-      const particleCount = 50 * (timeLeft / duration);
-
-      confetti({
-        ...defaults,
-        particleCount,
-        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-      });
-      confetti({
-        ...defaults,
-        particleCount,
-        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-      });
-    }, 250);
-  })();
+      if (timeLeft > 0) {
+        requestAnimationFrame(frame);
+      }
+    })();
+  }
 }
--- js/userscripts.js
+++ js/userscripts.js
@@ -11,9 +11,10 @@ export const userscripts = [
   {
     name: 'lara',
     ids: ['35da7902-93a2-4b82-b5f3-c518f392eb2f'],
-    script: function () {
-      document.querySelector('#input .reset').style.background = 'lightpink';
-    },
+    // script: function () {
+    //   document.querySelector('#input .reset').style.background = 'lightpink';
+    // },
+    script: birthday,
   },
   {
     name: 'filip',
@@ -22,13 +23,15 @@ export const userscripts = [
       '5290aa95-43cb-43ca-8fbc-ec6e5e2b0c98', // iphone pwa
       'a756086e-53c6-446a-a595-ed53e5328cb1', // mac safari
     ],
-    script: function () {
-      document.querySelector('#input .reset').style.background = 'lightblue';
-    },
+    // script: function () {
+    //   document.querySelector('#input .reset').style.background = 'lightblue';
+    // },
+    script: birthday,
   },
   {
     name: 'ana',
     ids: ['6a4fb73c-bacd-4ff5-87fb-a0061af05534'],
+    script: birthday,
   },
   {
     name: 'anina babica',
@@ -40,6 +43,6 @@ export const userscripts = [
       '51d24fa8-ab6e-4342-8e2a-4fc689d57124', // mac safari localhost
       'f5078f7c-fcdf-4a1c-9a8a-7c4e969d8024', // iphone localhost (pwa?)
     ],
-    // script: birthday,
+    script: birthday,
   },
 ];
